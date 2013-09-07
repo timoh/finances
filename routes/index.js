35ea2689f0ee2991afaf2f7068f55a29d17109bc
Timo H <timo@dealmachine.net>
2013-09-07 22:42:32 +0300

    added mongoose

--- routes/index.js
+++ routes/index.js
@@ -5,26 +5,70 @@
 
 var csv = require('csv');
 var fs = require('fs');
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+    mongoose.connect('mongodb://localhost/financials_test');
+
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function callback () {
+      console.log('DB connect successful!');
+    });
+
+var transactionSchema = mongoose.Schema({
+    date: Date,
+    recipient: String,
+    sum: String
+});
+
+var Transaction = mongoose.model('Transaction', transactionSchema);
+
 
 exports.index = function(req, res){
 
     csv().from.path(__dirname + '/paatili.csv', {delimiter: ';', escape: '"'}).to.array(function(data){
 
-        var tyypit = {};
-
         function printDataRow(element, index, array){
 
-            var nimi = element[1];
-            var summa = element[2];
-            tyypit[nimi] = tyypit[nimi]+1 || 1;
-            tyypit[nimi]["Summa"] = tyypit[nimi]["Summa"]+summa || summa;
+            Transaction.find({name: element[1]}, function (err, transaction) {
 
-            //console.log(element[1]);
+                function handleError(err) {
+                    console.log ("Error:", err);
+                }
+
+                if (err) return h
+
+                function addNew() {
+                    var newTrans = new Transaction({ date: moment(element[0]), recipient: element[1], sum: element[2]});
+
+                    newTrans.save(function(err) {
+                        if (err) return handleError(err);
+                        console.log('Transaction for '+ element[1] + ' saved!');
+                    })
+                }
+
+                if (transaction == element[1]){
+                    console.log('Transaction with the recipient of '+ element[1] + ' found.');
+                    // exists!
+
+                    if (transaction.date == element[0] && transaction.sum == element[2]) {
+                        // probably the same transaction, skip!
+                    } else {
+                        addNew();
+                    }
+
+                } else  {
+                    // does not exist!
+                    addNew();
+
+
+
+                } 
+            })
         }
 
         data.forEach(printDataRow);
-
-        console.log(tyypit);
 
     });
 
